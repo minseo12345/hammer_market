3ac0faba5ce87924058d39c57a5171afade482b9
최승훈 <gogo5896544@naver.com>
2024-12-18 15:46:54 +0900

    fix : transaction 스케줄러 이용한 로직 구현

--- src/main/java/com/hammer/hammer/auction/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hammer.hammer.auction.repository;
-
-import com.hammer.hammer.auction.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
