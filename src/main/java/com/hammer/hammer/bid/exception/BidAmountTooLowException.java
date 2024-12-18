04a459a17969e879c3481e89e62afa3df9ccf3f4
[CLOUD5] 박민서 <qkrals2475@naver.com>
2024-12-18 09:20:33 +0000

    Revert "Merge branch 'fix-path-ms' into 'master'"
    This reverts merge request !7

--- src/main/java/com/hammer/hammer/bid/exception/BidAmountTooLowException.java
+++
@@ -1,7 +0,0 @@
-package com.hammer.hammer.bid.exception;
-
-public class BidAmountTooLowException extends RuntimeException {
-    public BidAmountTooLowException(String message) {
-        super(message);
-    }
-}
