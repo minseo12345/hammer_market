19499685b6d4a92a6a775dda8b98d0c4062e31d5
[CLOUD5] 최승훈 <gogo5896544@naver.com>
2024-12-18 10:38:27 +0000

    Update Application.java

--- src/main/java/com/hammer/hammer/Application.java
+++ src/main/java/com/hammer/hammer/Application.java
@@ -2,7 +2,6 @@ package com.hammer.hammer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//오토컨피그
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
