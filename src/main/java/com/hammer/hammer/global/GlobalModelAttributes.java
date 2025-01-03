ee660c8f17655fc715535a29c09f4b66ee9469e3
최승훈 <gogo5896544@naver.com>
2025-01-03 10:30:15 +0900

    fix : 거래완료 포인트 증감 로직 구현

--- src/main/java/com/hammer/hammer/global/GlobalModelAttributes.java
+++ src/main/java/com/hammer/hammer/global/GlobalModelAttributes.java
@@ -1,34 +1,9 @@
-package com.hammer.hammer.fragments.layout;
+package com.hammer.hammer.global;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-/*@RequiredArgsConstructor
-@Controller
-@RequestMapping("/topbar")
-public class FragmentsController {
-
-//    private final UserService userService;
-
-    // 특정 요청에서만 userId를 모델에 추가
-    @RequestMapping
-    public String topbar(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication != null && authentication.getName() != null) {
-            try {
-                Long userId = Long.parseLong(authentication.getName());
-                model.addAttribute("userId", userId);
-            } catch (NumberFormatException e) {
-                System.err.println("Invalid userId format in authentication: " + authentication.getName());
-            }
-        }
-
-        return "fragments/topbar"; // topbar.html 템플릿
-    }
-}*/
 
 @ControllerAdvice
 public class GlobalModelAttributes {
