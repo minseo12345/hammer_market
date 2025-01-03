a3f986ab42d68f5143af2fd1b2db86c76010f361
jeonghak <kjh47345@naver.com>
2025-01-03 16:49:59 +0900

    item : 에러수정

--- src/main/java/com/hammer/hammer/chat/controller/WebSocketController.java
+++ src/main/java/com/hammer/hammer/chat/controller/WebSocketController.java
@@ -2,11 +2,16 @@ package com.hammer.hammer.chat.controller;
 
 import com.hammer.hammer.chat.entity.Message;
 import com.hammer.hammer.chat.service.ChatService;
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class WebSocketController {
@@ -14,7 +19,11 @@ public class WebSocketController {
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public Message sendMessage(Message message) {
+    public Message sendMessage(@Valid Message message, BindingResult result) {
+        if (result.hasErrors()) {
+            log.info("valid error occured: {}", result.getAllErrors());
+            throw new ValidationException(result.getAllErrors().toString());
+        }
         return chatService.saveMessage(message.getChatRoomId(), message.getSenderId(), message.getContent());
     }
 }
--- src/main/resources/static/js/chat/chat_script.js
+++ src/main/resources/static/js/chat/chat_script.js
@@ -243,6 +243,17 @@ messageInputEl.addEventListener('keypress', (event) => {
 // 메시지 전송 기능
 function sendMessage() {
     const content = messageInputEl.value.trim();
+    // 메시지가 비어있을 경우
+    if (content === "") {
+        alert("메세지를 입력해주세요.");
+        return;
+    }
+
+    // 메시지가 100자 이상일 경우
+    if (content.length > 100) {
+        alert("메세지는 100자 까지만 가능합니다.");
+        return;
+    }
     if (content && currentChatRoomId) {
         const message = {
             chatRoomId: currentChatRoomId,
